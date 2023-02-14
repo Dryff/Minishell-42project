312a6198b273cd05b0446407eb7376c0f6a0c41b
Maxime Finette <mfinette@z4r11p3.42lyon.fr>
2023-02-14 10:11:08 +0100

    working unset

--- builtins/unset.c
+++ builtins/unset.c
@@ -12,13 +12,48 @@
 
 #include "../msh.h"
 
-void    ft_unset(t_env *env, char *cmd)
-{
-    (void)env;
-    (void)cmd;
-}
-
 char    *get_unset_cmd(char *line)
 {
-    return (line + 4);
+    return (line + 6);
+}
+
+void    remove_line_from_tab(char **tab, int index)
+{
+    while (tab[index])
+    {
+        tab[index] = tab[index + 1];
+        index++;
+    }
+    tab[index - 1] = NULL;
+}
+
+int	get_cmd_index(char **tab, char *cmd)
+{
+    int i;
+    int sz;
+
+    i = 0;
+    sz = ft_strlen(cmd);
+    while (tab[i])
+    {
+        if (ft_strstr(tab[i], cmd))
+        {
+            if (tab[i][0] == cmd[0] && (tab[i][sz] == '=' || !tab[i][sz]))
+                return (i);
+        }
+        i++;
+    }
+    return (-1);
+}
+
+void    ft_unset(t_env *env, char *cmd)
+{
+    int i;
+
+    i = get_cmd_index(env->tab, cmd);
+    if (i >= 0)
+        remove_line_from_tab(env->tab, i);
+    i = get_cmd_index(env->sort_tab, cmd);
+    if (i >= 0)
+        remove_line_from_tab(env->sort_tab, i);
 }
