e66d7e20bdcd08940f946cfae80edef0d5c3e991
Maxime Finette <mfinette@z4r11p3.42lyon.fr>
2023-04-09 00:05:37 +0200

    putain de segfolt et de gestion d'erreur du flag - sa mere

--- builtins/dvd.c
+++ builtins/dvd.c
@@ -6,7 +6,7 @@
 /*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 10:22:55 by mfinette          #+#    #+#             */
-/*   Updated: 2023/04/03 15:19:02 by mfinette         ###   ########.fr       */
+/*   Updated: 2023/04/09 00:04:56 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,63 +32,90 @@ void	update_pwd_env(t_msh *msh, char *actual_path)
 	free(oldpwd);
 }
 
-void	chdir_home_old(t_msh *msh, int code)
+void	chdir_home(t_msh *msh)
 {
 	int		error;
 	char	*actual_path;
+	char	*tmp;
 	
 	error = 0;
+	tmp = ft_expand(&msh->env, "HOME");
 	actual_path = getcwd(NULL, 0);
-	if (code == HOME)
+	if (!tmp)
 	{
-		if (chdir(ft_expand(&msh->env, "HOME")) != 0)
-		{
-			error = 1;
-			ft_err_printf("cd: HOME not set\n");
-			update_msh_status(1);
-		}
+		error = 1;
+		ft_err_printf("cd: HOME not set\n");
+		update_msh_status(1);
 	}
-	else if (code == OLD)
+	else if (chdir(ft_expand(&msh->env, "HOME")) != 0)
 	{
-		if (chdir(ft_expand(&msh->env, "OLDPWD")) != 0)
-		{
-			error = 1;
-			ft_err_printf("cd: OLDPWD not set\n");
-			update_msh_status(1);
-		}
-		else
-			printf("%s\n", ft_expand(&msh->env, "OLDPWD"));
+		error = 1;
+		ft_err_printf("msh: cd: %s: No such file or directory\n", tmp);
+		update_msh_status(1);
 	}
 	if (!error)
 		update_pwd_env(msh, actual_path);
 	free(actual_path);
 }
 
-void	ft_dvd(t_msh *msh, int cmd_id)
+void	chdir_old(t_msh *msh)
 {
+	int		error;
+	char	*actual_path;
+	char	*tmp;
+	
+	error = 0;
+	actual_path = getcwd(NULL, 0);
+	tmp = ft_expand(&msh->env, "OLDPWD");
+	if (!tmp)
+	{
+		error = 1;
+		ft_err_printf("cd: OLDPWD not set\n");
+		update_msh_status(1);
+	}
+	else if (chdir(ft_expand(&msh->env, "OLDPWD")) != 0)
+	{
+		error = 1;
+		ft_err_printf("msh: cd: %s: No such file or directory\n", tmp);
+		update_msh_status(1);
+	}
+	else
+		printf("%s\n", ft_expand(&msh->env, "OLDPWD"));
+	if (!error)
+		update_pwd_env(msh, actual_path);
+	free(actual_path);
+}
+
+void	real_pwd(t_msh *msh, char *cmd)
+{	
+	char	*tmp;
 	char	*actual_path;
 	char	*new_path;
-	char	*tmp;
 
-	if (!msh->cmd[cmd_id].param[1])
-		chdir_home_old(msh, HOME);
-	else if (msh->cmd[cmd_id].param[1][0] == '-')
-		chdir_home_old(msh, OLD);
-	else
+	actual_path = getcwd(NULL, 0);
+	new_path = ft_strjoin(actual_path, "/");
+	tmp = ft_strjoin(new_path, cmd);
+	if (chdir(tmp) != 0)
 	{
-		actual_path = getcwd(NULL, 0);
-		new_path = ft_strjoin(actual_path, "/");
-		tmp = ft_strjoin(new_path, msh->cmd[cmd_id].param[1]);
-		if (chdir(tmp) != 0)
-		{
-			ft_err_printf("cd: %s: No such file or directory\n", msh->cmd[cmd_id].param[1]);
-			free(actual_path);
-			update_msh_status(1);
-		}
-		else
-			update_pwd_env(msh, actual_path);
-		free(new_path);
-		free(tmp);
-		free(actual_path);
+		ft_err_printf("cd: %s: No such file or directory\n", cmd);
+		update_msh_status(1);
 	}
+	else
+		update_pwd_env(msh, actual_path);
+	free(new_path);
+	free(tmp);
+	free(actual_path);	
+}
+
+void	ft_dvd(t_msh *msh, int cmd_id)
+{
+	char	*cmd;
+
+	cmd = msh->cmd[cmd_id].param[1];
+	if (!cmd)
+		chdir_home(msh);
+	else if (cmd[0] == '-' && !cmd[1])
+		chdir_old(msh);
+	else
+		real_pwd(msh, cmd);
 }
