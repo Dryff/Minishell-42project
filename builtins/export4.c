375d2d17f9dc90a59fe36f40c316f1b9d419130b
Maxime Finette <mfinette@z4r11p1.42lyon.fr>
2023-04-15 16:55:24 +0200

    plus export fix

--- builtins/export4.c
+++ builtins/export4.c
@@ -6,23 +6,54 @@
 /*   By: mfinette <mfinette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 00:05:55 by mfinette          #+#    #+#             */
-/*   Updated: 2023/04/15 16:48:51 by mfinette         ###   ########.fr       */
+/*   Updated: 2023/04/15 16:54:44 by mfinette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../msh.h"
 
+char	*get_plus_export_cmd(char *cmd)
+{
+	char	*dup;
+	char	*dup2;
+	char	*dup3;
+
+	dup = ft_substr(cmd, 0, ft_strlen_until(cmd, '=') - 1);
+	if (!dup)
+		return (NULL);
+	dup2 = ft_strjoin(dup, "=");
+	if (!dup2)
+		return (NULL);
+	dup3 = ft_substr(cmd, ft_strlen_until(cmd, '=') + 1, ft_strlen(cmd));
+	if (!dup3)
+		return (NULL);
+	free(dup);
+	dup = ft_strjoin(dup2, dup3);
+	if (!dup)
+		return (NULL);
+	free(dup2);
+	free(dup3);
+	return (dup);
+}
+
 int	plus_export(t_msh *msh, char *cmd)
 {
 	char	*purecmd;
 	char	*dup;
+	char	*export_cmd;
 
 	dup = ft_substr(cmd, 0, ft_strlen_until(cmd, '=') - 1);
 	if (!dup)
 		return (msh->error = 1, 0);
 	purecmd = ft_expand(&msh->env, dup);
 	if (!purecmd)
-		complete_export(msh, cmd);
+	{
+		export_cmd = get_plus_export_cmd(cmd);
+		if (!export_cmd)
+			return (msh->error = 1, 0);
+		complete_export(msh, export_cmd);
+		free(export_cmd);
+	}
 	else
 		actual_plus_export(msh, dup, cmd, purecmd);
 	free(dup);
